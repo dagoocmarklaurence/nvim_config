c296f23adb73be539c71dc151a97abe3dfff6367
laurence <dagoocmarklaurence@gmail.com>
2025-07-18 19:41:22 +0800

    Initial Commit

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,25 @@
+return {
+	"goolord/alpha-nvim",
+	--https://github.com/MaximilianLloyd/ascii.nvim/tree/master
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.startify")
+
+		dashboard.section.header.val = {
+			[[                                                    ]],
+			[[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
+			[[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
+			[[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
+			[[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
+			[[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
+			[[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
+			[[                                                    ]],
+		}
+
+		alpha.setup(dashboard.opts)
+	end,
+}
